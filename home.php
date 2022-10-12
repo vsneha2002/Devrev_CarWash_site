67753d433f1a96ae508590c19552dfa90cc147f9
Sneha Vijaykumar <71842875+vsneha2002@users.noreply.github.com>
2022-10-12 11:38:34 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,57 @@
+ <!-- Header-->
+ <header class="bg-dark py-5" id="main-header">
+    <div class="container h-100 d-flex align-items-end justify-content-center w-100">
+        <div class="text-center text-white w-100">
+            <h1 class="display-4 fw-bolder">Car wash</h1>
+            <p class="lead fw-normal text-white-50 mb-0">We will take care of your vehicle</p>
+            <div class="col-auto mt-2">
+                <button class="btn btn-primary btn-lg rounded-0" id="send_request" type="button">Send Service Request</button>
+            </div>
+        </div>
+    </div>
+</header>
+<!-- Section-->
+
+<script>
+    $(function(){
+        $('#search').on('input',function(){
+            var _search = $(this).val().toLowerCase().trim()
+            $('#service_list .item').each(function(){
+                var _text = $(this).text().toLowerCase().trim()
+                    _text = _text.replace(/\s+/g,' ')
+                    console.log(_text)
+                if((_text).includes(_search) == true){
+                    $(this).toggle(true)
+                }else{
+                    $(this).toggle(false)
+                }
+            })
+            if( $('#service_list .item:visible').length > 0){
+                $('#noResult').hide('slow')
+            }else{
+                $('#noResult').show('slow')
+            }
+        })
+        $('#service_list .item').hover(function(){
+            $(this).find('.callout').addClass('shadow')
+        })
+        $('#service_list .view_service').click(function(){
+            uni_modal("Service Details","view_service.php?id="+$(this).attr('data-id'),'mid-large')
+        })
+        $('#send_request').click(function(){
+            uni_modal("Fill the Service Request Form","send_request.php",'large')
+        })
+
+    })
+    $(document).scroll(function() { 
+        $('#topNavBar').removeClass('bg-transparent navbar-dark bg-primary')
+        if($(window).scrollTop() === 0) {
+           $('#topNavBar').addClass('navbar-dark bg-transparent')
+        }else{
+           $('#topNavBar').addClass('navbar-dark bg-primary')
+        }
+    });
+    $(function(){
+        $(document).trigger('scroll')
+    })
+</script>
